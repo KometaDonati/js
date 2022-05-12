321242ff2ce2950be1ab49413738c45435073228
Ariadna <98162619+KometaDonati@users.noreply.github.com>
2022-05-12 22:43:11 +0500

    Create let.js

---
+++ let.js
@@ -0,0 +1,11 @@
+let sofa4 = 4;
+let sofa6 = 6;
+let sofa_name = " blue"
+let result = sofa4 + sofa6;
+let result2 = sofa6 + sofa_name;
+
+console.log("result " + sofa4 + sofa6)
+
+
+console.log(result);
+console.log(result2);
